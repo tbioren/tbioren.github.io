3fcb93918019578a3148c152a2e0ec5beeccce81
tbioren <tbioren22@gmail.com>
2023-12-11 12:31:00 -0500

    removed unnecessary imports

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,7 @@
-import 'dart:io';
-
-import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:auto_size_text/auto_size_text.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:url_launcher/url_launcher.dart';
-import 'package:url_launcher/url_launcher_string.dart';
 
 void main() {
   runApp(const MyApp());
