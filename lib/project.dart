a3600b56efcc0e11f9ef74837aaa75a10c979d1c
tbioren <tbioren22@gmail.com>
2023-12-14 19:26:48 -0500

    complete restructure

--- lib/project.dart
+++ lib/project.dart
@@ -1,11 +1,14 @@
 class Project {
   String title = "";
   String description = "";
+  Uri link = Uri();
+  List<String> tags = List<String>.empty(growable: true);
 
   Project(this.title, this.description);
 
   Project.fromJson(Map<String, dynamic> json) {
     title = json['title'];
     description = json['description'];
+    link = Uri.parse(json['link']);
   }
 }
