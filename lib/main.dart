07c059f36b991d4362c01b3019c98c6c4ae8b50a
tbioren <tbioren22@gmail.com>
2023-12-15 19:09:42 -0500

    idk anymore

--- lib/main.dart
+++ lib/main.dart
@@ -8,21 +8,24 @@ void main() {
 class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
+  @override
   HomePage createState() => HomePage();
 }
 
 class HomePage extends State<MyApp> {
   String _aboutFile = '';
+
+  @override
+  void initState() {
+    super.initState();
+    loadAssets();
+  }
+
   Future<void> loadAssets() async {
     String fileText = await rootBundle.loadString('data/aboutMe.txt');
     setState(() {
       _aboutFile = fileText;
     });
-  }
-
-  @override
-  void initState() {
-    loadAssets();
   }
 
   // This widget is the root of your application.
@@ -37,80 +40,140 @@ class HomePage extends State<MyApp> {
           scrollDirection: Axis.vertical,
           children: <Widget>[
             // Title Screen
-            SizedBox(
-              width: MediaQuery.of(context).size.width * 0.75,
-              height: MediaQuery.of(context).size.height,
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text(
-                    "Thomas Bioren",
-                    style: TextStyle(
-                      color: Colors.black,
-                      fontStyle: FontStyle.normal,
-                      fontWeight: FontWeight.bold,
-                      fontSize: MediaQuery.of(context).size.width / 20,
-                    ),
-                  ),
-                ],
-              ),
-            ),
+            TitleScreen(),
             // About
-            Container(
-              color: const Color.fromARGB(255, 255, 255, 250),
-              child: SizedBox(
-                height: MediaQuery.of(context).size.height,
-                child: Padding(
-                  padding: const EdgeInsets.only(top: 100),
-                  child: Column(
-                    children: [
-                      Text(
-                        "About Me",
-                        style: TextStyle(
-                          color: Colors.black,
-                          fontStyle: FontStyle.normal,
-                          fontWeight: FontWeight.bold,
-                          fontSize: MediaQuery.of(context).size.width / 50,
-                        ),
-                      ),
-                      SizedBox(
-                        width: MediaQuery.of(context).size.width * 0.5,
-                        child: Text(
-                          _aboutFile,
-                          textAlign: TextAlign.center,
-                          style: TextStyle(
-                            color: Colors.grey,
-                            fontStyle: FontStyle.normal,
-                            fontWeight: FontWeight.bold,
-                            fontSize: MediaQuery.of(context).size.width / 75,
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-            ),
+            About(aboutFile: _aboutFile),
             // Footer
-            SizedBox(
-              width: MediaQuery.of(context).size.width,
-              height: MediaQuery.of(context).size.height / 5,
-              child: Container(
-                color: const Color.fromARGB(255, 240, 240, 240),
-                child: Center(
-                  child: Text(
-                    "Placeholder Footer",
-                    style: TextStyle(
-                      color: Colors.black,
-                      fontStyle: FontStyle.normal,
-                      fontWeight: FontWeight.bold,
-                      fontSize: MediaQuery.of(context).size.width / 50,
-                    ),
+            Footer(),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class TitleScreen extends StatefulWidget {
+  @override
+  State<TitleScreen> createState() => _TitleScreenState();
+
+  const TitleScreen({
+    super.key,
+  });
+}
+
+class _TitleScreenState extends State<TitleScreen> {
+  double _nameOpacity = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    Future.delayed(const Duration(milliseconds: 0), () {
+      _nameOpacity = 1;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: MediaQuery.of(context).size.width * 0.75,
+      height: MediaQuery.of(context).size.height,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          AnimatedOpacity(
+            opacity: _nameOpacity,
+            duration: const Duration(milliseconds: 500),
+            child: Text(
+              "Thomas Bioren",
+              style: TextStyle(
+                color: Colors.black,
+                fontStyle: FontStyle.normal,
+                fontWeight: FontWeight.bold,
+                fontSize: MediaQuery.of(context).size.width / 20,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class Footer extends StatelessWidget {
+  const Footer({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: MediaQuery.of(context).size.width,
+      height: MediaQuery.of(context).size.height / 5,
+      child: Container(
+        color: const Color.fromARGB(255, 240, 240, 240),
+        child: Center(
+          child: Text(
+            "Placeholder Footer",
+            style: TextStyle(
+              color: Colors.black,
+              fontStyle: FontStyle.normal,
+              fontWeight: FontWeight.bold,
+              fontSize: MediaQuery.of(context).size.width / 50,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class About extends StatefulWidget {
+  const About({
+    super.key,
+    required String aboutFile,
+  }) : _aboutFile = aboutFile;
+
+  final String _aboutFile;
+
+  @override
+  State<About> createState() => _AboutState();
+}
+
+class _AboutState extends State<About> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: const Color.fromARGB(255, 255, 255, 250),
+      child: SizedBox(
+        height: MediaQuery.of(context).size.height,
+        child: Padding(
+          padding: const EdgeInsets.only(top: 100),
+          child: Column(
+            children: [
+              Text(
+                "About Me",
+                style: TextStyle(
+                  color: Colors.black,
+                  fontStyle: FontStyle.normal,
+                  fontWeight: FontWeight.bold,
+                  fontSize: MediaQuery.of(context).size.width / 50,
+                ),
+              ),
+              SizedBox(
+                width: MediaQuery.of(context).size.width * 0.5,
+                child: Text(
+                  widget._aboutFile,
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    color: Colors.grey,
+                    fontStyle: FontStyle.normal,
+                    fontWeight: FontWeight.bold,
+                    fontSize: MediaQuery.of(context).size.width / 75,
                   ),
                 ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
